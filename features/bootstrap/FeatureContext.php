5e52547c8fe630cea6f1a1a1b3d86fda0e5fbdef
Daniel Medeiros <danielmmf@gmail.com>
2017-05-03 20:54:40 -0300

    bdd rodando com um codigo de testes

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
@@ -18,5 +19,53 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given I am a player
+     */
+    public function iAmAPlayer()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I have some data to send
+     */
+    public function iHaveSomeDataToSend(TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I send a POST request to :arg1
+     */
+    public function iSendAPostRequestTo($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the response status code should be :arg1
+     */
+    public function theResponseStatusCodeShouldBe($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the response should contain a :arg1 entry
+     */
+    public function theResponseShouldContainAEntry($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the response entry :arg1 should be :arg2
+     */
+    public function theResponseEntryShouldBe($arg1, $arg2)
+    {
+        throw new PendingException();
     }
 }
---
+++ features/game.feature
@@ -0,0 +1,13 @@
+Feature: Create Game
+    In order to play a new game
+    As a player
+    I need to able to create a new game
+
+    Scenario: Cerate a new game
+        Given I am a player
+        And I have some data to send
+        | username | foo |
+        When I send a POST request to "/games"
+        Then the response status code should be 201
+        And the response should contain a "message" entry
+        And the response entry "message" should be "created"
